45c4842d4a3beaad78471399224732e4802de165
Yura Ilchenko <yurailche@gmail.com>
2023-07-19 11:55:04 +0300

    Add database and create admin panel for the appointments

--- PETlya/PETlyaAPP/models.py
+++ PETlya/PETlyaAPP/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    pet_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=13)
+    problem = models.TextField(blank=True)
+
