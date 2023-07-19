a4a8f7db7bcacc08d6bf2e99e2d86f662768dcb8
Yura Ilchenko <yurailche@gmail.com>
2023-07-20 01:19:44 +0300

    new features

--- PETlya/PETlyaAPP/views.py
+++ PETlya/PETlyaAPP/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 from .models import Appointment
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'title': 'PETlya'})
 
 def home_ua(request):
-    return render(request, 'index_ua.html')
+    return render(request, 'index_ua.html', {'title': 'PETlya'})
+
 
 def appoint(request):
     if request.method == 'POST':
@@ -22,5 +23,5 @@ def appoint(request):
             new_appointment = Appointment(name=name, surname=surname, pet_name=pet_name, phone_number=phone_number, problem=problem)
             new_appointment.save()
 
-    return render(request, 'appointment.html')
+    return render(request, 'appointment.html', {'title': 'Appointment'})
 
